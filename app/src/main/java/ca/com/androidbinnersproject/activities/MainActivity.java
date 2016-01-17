7371a9cab5240edd4aaebe858fdd8908b3120947
jhowcs <jhowcs@gmail.com>
2016-01-17 12:51:28 -0200

    Login + projects' structure

---
+++ app/src/main/java/ca/com/androidbinnersproject/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package ca.com.androidbinnersproject.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.com.androidbinnersproject.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
