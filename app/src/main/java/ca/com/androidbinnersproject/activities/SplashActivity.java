194e2d17f73c70112de54fa7b9eda69c6ecbc9c4
jhowcs <jhowcs@gmail.com>
2016-02-08 21:09:17 -0200

    I changed the flow of app start (Splash->StartApp->Login->AboutActivity).

--- app/src/main/java/ca/com/androidbinnersproject/activities/SplashActivity.java
+++ app/src/main/java/ca/com/androidbinnersproject/activities/SplashActivity.java
@@ -13,7 +13,7 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, AboutAppActivity.class);
+        Intent intent = new Intent(this, StartApp.class);
         startActivity(intent);
         finish();
     }
